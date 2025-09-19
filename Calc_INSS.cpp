d653342ad0edc92a61ed88dde4bf4298dcb607a0
felipemtdev <felipemoraesteles6@gmail.com>
2025-09-19 07:59:22 -0300

    Update Calc_INSS.cpp

--- Calc_INSS.cpp
+++ Calc_INSS.cpp
@@ -1,45 +1,25 @@
 float inss(float salario_bruto){
- 
-   float resultado;
- 
+   float inss;
    if (salario_bruto > 0){
- 
        if(salario_bruto >= 1 && salario_bruto <=1518){
-           
-           resultado =  salario_bruto * 0.075;
-          
+           inss =  salario_bruto * 0.075;
        }else if (salario_bruto >= 1518.01 && salario_bruto <= 2793.88){
-           
-           resultado = salario_bruto * 0.09;
-          
+           inss = salario_bruto * 0.09;
        }else if (salario_bruto >= 2793.89 && salario_bruto <= 4190.83){
-           
-           resultado = salario_bruto * 0.12;
-          
+           inss = salario_bruto * 0.12;
        }else if (salario_bruto >= 4190.84 && salario_bruto <= 8157.41){
-           
-            resultado = salario_bruto * 0.14;      
-         	
+           inss = salario_bruto * 0.14;      
        }else{
- 
-           resultado = 1142.04;   
- 
+           inss = 1142.04;
        }
- 
    }else{
- 
-       resultado = -1;
+       inss = -1;
    }
-        return resultado;
- 
+        return inss;
 }
  
 main(){
- 
-   float a = 1800,  i;
- 
+   float a =0,  i;
    i = inss(a);
- 
    printf("resposta: %f", i);
- 
 }
